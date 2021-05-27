8677bc37ba9ffeabc7b708474bcff1bb45203c2c
Stefan Untura <u_stefan@mail.com>
2021-05-27 09:26:52 +0200

    deploy api

--- restApi.py
+++ restApi.py
@@ -3,15 +3,13 @@ import MySQLdb
 import re
 from MySQLdb import _mysql
 from MySQLdb.constants import FIELD_TYPE
-from flask import request, jsonify, json
-from flask import make_response
+from flask import request, jsonify, json, make_response, render_template
 
 app = flask.Flask(__name__);
 app.config["DEBUG"] = True
 
-homePageText = "<h1><center>FokusApp Rest Api</center></h1> <h3>Options:</h3> <ul> <li>/api/loginInfo/all - <i>Fetches all login data from database</i></li> </ul>"
-
 #=============================================================================#Global, frequently used functions========================================================================
+
 def connect_db():
 
     global db
@@ -57,18 +55,27 @@ def prepareResultedQueryString(queryResult):
     return modifiedQueryResult
 
 #===================================================================================#Home page of the API===============================================================================
+
 @app.route('/', methods=['GET'])
 
 def home():
-    return homePageText
+    return render_template("index.html")
 
 #=============================================================================#Requesting QuoteInfo from the API========================================================================
+
 @app.route('/quoteInfo', methods=['GET'])
 
 def quoteInfo():
+    quoteID = request.args.get("quoteID")
+
     connect_db()
 
-    quoteInfoQuery = """SELECT * FROM quoteoftheday"""
+    if quoteID is None:
+        quoteInfoQuery = """SELECT * FROM quoteoftheday"""
+        print("quoteID IS NONE")
+    else:
+        quoteInfoQuery = """SELECT * FROM quoteoftheday WHERE quoteDay_key = '{}' """.format(quoteID)
+        print("quoteID HAS A VALUE")
 
     fetchDatabaseInfo(quoteInfoQuery)
     prepareResultedQueryString(convertedResult)
@@ -88,6 +95,7 @@ def quoteInfo():
 
     return quoteInfoJsonObject
 #============================================================================#Requesting QuoteInfo from the API=========================================================================
+
 @app.route('/appInfo', methods=['GET'])
 
 def appInfo():
@@ -112,6 +120,123 @@ def appInfo():
     close_db()
 
     return appInfoJsonObject
+
+
+
+#============================================================================#Requesting NotesInfo from the API=========================================================================
+
+@app.route('/notesInfo', methods=['GET'])
+
+def notesInfo():
+    noteID = request.args.get("noteID")
+    email = request.args.get("email")
+
+    connect_db()
+
+    if email is None or noteID is None:
+        notesInfoQuery = """SELECT * FROM notes"""
+        print("email or noteID IS NONE")
+        print(email)
+        print(noteID)
+    else:
+        notesInfoQuery = """SELECT * FROM notes WHERE email = '{}' AND note_id='{}' """.format(email, noteID)
+        print("email and noteID HAVE A VALUE")
+
+
+
+    fetchDatabaseInfo(notesInfoQuery)
+    prepareResultedQueryString(convertedResult)
+
+    note_id = modifiedQueryResult[0]
+    email = modifiedQueryResult[1]
+    title = modifiedQueryResult[2]
+
+    notesData = {
+        "Note_ID": note_id,
+        "Email": email,
+        "Title": title
+    }
+
+    notesInfoJsonObject = jsonify(notesData)
+    print("Successfully jsonified the substringed result")
+
+    close_db()
+
+    return notesInfoJsonObject
+#============================================================================#Requesting ScheduleInfo from the API=========================================================================
+
+@app.route('/scheduleInfo', methods=['GET'])
+
+def scheduleInfo():
+    connect_db()
+
+    scheduleInfoQuery = """SELECT * FROM schedule"""
+
+    fetchDatabaseInfo(scheduleInfoQuery)
+    prepareResultedQueryString(convertedResult)
+
+    timedate = modifiedQueryResult[0]
+    email = modifiedQueryResult[1]
+    eventName = modifiedQueryResult[2]
+    timerDuration = modifiedQueryResult[3]
+    timerQuantity = modifiedQueryResult[4]
+    eventStatus = modifiedQueryResult[5]
+
+    scheduleData = {
+        "Timedate": timedate,
+        "Email": email,
+        "Eventname": eventName,
+        "TimerDuration": timerDuration,
+        "TimerQuantity": timerQuantity,
+        "EventStatus": eventStatus
+    }
+
+    scheduleInfoJsonObject = jsonify(scheduleData)
+    print("Successfully jsonified the substringed result")
+
+    close_db()
+
+    return schduleInfoJsonObject
+
+#============================================================================#Upload .txt file to SQL=========================================================================
+
+@app.route('/uploadNotes', methods=['POST', 'GET'])
+def upload():
+    if request.method == "POST":
+        file = request.files['inputFile']
+
+        connect_db()
+
+        insertNotesFileQuery = """INSERT INTO notes(title, content) VALUES('{}', '{}')""".format(file.filename, file.read())
+
+        db.query(insertNotesFileQuery)
+        print("Successfully inserted the notes")
+
+        close_db()
+
+        return flask.redirect(flask.url_for("home"))
+    else:
+        return render_template("fileupload.html")
+
+#============================================================================#Update eventStatus column in schedule tableL=========================================================================
+
+@app.route('/scheduleUpdate', methods=['GET'])
+def scheduleUpdate():
+    email = request.args.get("email")
+    eventStatus = request.args.get("eventStatus")
+
+    connect_db()
+
+    updateEventStatusQuery = """UPDATE schedule SET eventStatus = '{}' WHERE email = '{}' """.format(eventStatus, email)
+
+    db.query(updateEventStatusQuery)
+    print("Successfully updated the eventStatus field")
+
+    close_db()
+
+    return flask.redirect(flask.url_for("home"))
+
+#============================================================================#Errorhandlers=========================================================================
 
 @app.errorhandler(404)
 def not_found(error):
